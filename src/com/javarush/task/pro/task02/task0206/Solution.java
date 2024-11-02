140da564fa0a16200905ee3799bf572624b935e9
DmitryKokhna <dmitry.kokhna@gmail.com>
2024-11-02 22:36:27 +0300

    02_Nov_2024 com.javarush.task.pro.task02.task0206 «Я снова по объявлению…»

---
+++ src/com/javarush/task/pro/task02/task0206/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.pro.task02.task0206;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+
+        /*
+В методе main объяви переменные word, phrase, line и text типа String.
+Присвой им разные значения. Используй одну команду для создания и инициализации переменных.
+Значениями могут быть любые строки.
+
+Требования:
+•	В методе main объяви четыре переменные типа String.
+•	Назови переменные именами word, phrase, line и text.
+•	Переменным сразу присвой значения.
+•	У всех переменных должны быть разные значения.
+
+
+         */
+        String word = "w", phrase = "p", line = "l", text = "t";
+    }
+}
