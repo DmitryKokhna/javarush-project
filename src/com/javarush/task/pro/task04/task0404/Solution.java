be88003cf8828c70afaaed2b0fdfe8a02dd0b6ef
DmitryKokhna <dmitry.kokhna@gmail.com>
2024-11-24 19:42:20 +0300

    24_Nov_2024 com.javarush.task.pro.task04.task0404 "Заполненный прямоугольник"

---
+++ src/com/javarush/task/pro/task04/task0404/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.pro.task04.task0404;
+
+/* 
+Заполненный прямоугольник
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        int stringCount = 0;
+
+        while (stringCount < 5) {
+            int columnCount = 0;
+
+            while (columnCount < 10) {
+                System.out.print("Q");
+                columnCount++;
+            }
+            System.out.println();
+            stringCount++;
+
+        }
+
+    }
+}
