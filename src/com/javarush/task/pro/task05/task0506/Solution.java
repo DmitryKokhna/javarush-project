72c55b63ff4cd84e1373bea9f8c029c44561d516
DmitryKokhna <dmitry.kokhna@gmail.com>
2024-12-02 16:52:41 +0300

    02_Dec_2024 Create new scratch file from selection "Минимальное из N чисел"

---
+++ src/com/javarush/task/pro/task05/task0506/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.task.pro.task05.task0506;
+
+import java.util.Scanner;
+
+/* 
+Минимальное из N чисел
+*/
+
+public class Solution {
+    public static int[] array;
+
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        array = new int[n];
+        int minValue;
+
+        for (int i = 0; i < n; i++) {
+            array[i] = Integer.parseInt(scanner.nextLine());
+        }
+        minValue = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < minValue) {
+                minValue = array[i];
+            }
+        }
+        System.out.println(minValue);
+    }
+}
