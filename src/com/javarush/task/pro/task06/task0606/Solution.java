5f100896f23faf1b3fe432647bdcf4229e810b62
DmitryKokhna <dmitry.kokhna@gmail.com>
2025-01-07 18:47:08 +0300

    07_Jan_2025 com.javarush.task.pro.task06.task0606 "Универсальный солдат"

---
+++ src/com/javarush/task/pro/task06/task0606/Solution.java
@@ -0,0 +1,55 @@
+package com.javarush.task.pro.task06.task0606;
+
+/* 
+Универсальный солдат
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void universalMethod() {
+
+    }
+
+    //напишите тут ваш код
+    public static void universalMethod(int a2) {
+
+    }
+
+    public static void universalMethod(int a3, int a4) {
+
+    }
+
+    public static void universalMethod(int a3, int a4, int a5) {
+
+    }
+
+    public static void universalMethod(int a6, double db) {
+
+    }
+
+    public static void universalMethod(int a6, double db, String str7) {
+
+    }
+
+    public static void universalMethod(int a6, double db, String str7, boolean bl8) {
+
+    }
+
+    public static void universalMethod(float fl9) {
+
+    }
+
+    public static void universalMethod(float fl10, int in10) {
+
+    }
+
+    public static void universalMethod(float fl10, int[] ar1) {
+
+    }
+
+  }
+
