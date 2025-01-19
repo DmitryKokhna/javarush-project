fc457dc2692ffaae86b1a92704c5a4ef1eba36ab
DmitryKokhna <dmitry.kokhna@gmail.com>
2025-01-19 22:08:11 +0300

    19_Jan_2025 com.javarush.task.pro.task07.task0701 "Все возможные типы"

---
+++ src/com/javarush/task/pro/task07/task0701/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task07.task0701;
+
+/* 
+Все возможные типы
+*/
+
+public class Solution {
+    //напишите тут ваш код
+    byte a = 1;
+    short b = 1;
+    int c = 1;
+    long d = 1L;
+    float e = 2.0F;
+    double f = 2.0D;
+    boolean h = true;
+    char j = 'a';
+}
