f3cf5e0f51ed7249be7201509e81a0194fa980a2
DmitryKokhna <dmitry.kokhna@gmail.com>
2025-01-21 22:56:41 +0300

    21_Jan_2025 com.javarush.task.pro.task07.task0712 "Первые объекты"

---
+++ src/com/javarush/task/pro/task07/task0712/Solution.java
@@ -0,0 +1,35 @@
+package com.javarush.task.pro.task07.task0712;
+
+/* 
+Первые объекты
+*/
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.security.Signature;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        String string1 = new String("1");
+        String string2 = new String("2");
+        String string3 = new String("3");
+        String string4 = new String("4");
+        String string5 = new String("5");
+        String string6 = new String("6");
+        String string7 = new String("7");
+        String string8 = new String("8");
+        String string9 = new String("9");
+        String string10 = new String("10");
+
+        int[] array1 = new int[1];
+        int[] array2 = new int[2];
+        int[] array3 = new int[3];
+        int[] array4 = new int[4];
+        int[] array5 = new int[5];
+
+        Scanner scanner1 = new Scanner(System.in);
+        Scanner scanner2 = new Scanner("new");
+    }
+}
