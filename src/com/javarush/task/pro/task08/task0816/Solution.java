28df161e0b31f793b94535944229175c035c96be
DmitryKokhna <dmitry.kokhna@gmail.com>
2025-02-27 23:50:01 +0300

    27_Feb_2025 com.javarush.task.pro.task08.task0816 "Приоритеты"

--- src/com/javarush/task/pro/task08/task0816/Solution.java
+++ src/com/javarush/task/pro/task08/task0816/Solution.java
@@ -8,6 +8,7 @@ public class Solution {
 
     public static void main(String[] args) {
         int number = 2;
+        // скобки
         System.out.println(number + number * (number + ++number));
     }
 
