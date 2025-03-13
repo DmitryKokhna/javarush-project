8270cac0de0156fe8f5663b5809894d47b2b7b84
DmitryKokhna <dmitry.kokhna@gmail.com>
2025-03-13 22:45:32 +0300

    13_Mar_2025 com.javarush.task.pro.task09.task0912 "Проверка URL-адреса"

---
+++ src/com/javarush/task/pro/task09/task0912/Solution.java
@@ -0,0 +1,48 @@
+package com.javarush.task.pro.task09.task0912;
+
+/* 
+Проверка URL-адреса
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String[] urls = {"https://javarush.ru", "https://google.com", "http://wikipedia.org", "facebook.com", "https://instagram", "codegym.cc"};
+        for (String url : urls) {
+            String protocol = checkProtocol(url);
+            String domain = checkDomain(url);
+
+
+            System.out.println("У URL-адреса - " + url + ", сетевой протокол - " + protocol + ", домен - " + domain);
+        }
+    }
+
+    public static String checkProtocol(String url) {
+        //напишите тут ваш код
+        String protocol;
+        if (url.regionMatches(0, "https", 0, 5)) {
+            protocol = "https";
+        } else if (url.regionMatches(0, "http", 0, 4)) {
+            protocol = "http";
+        } else {
+            protocol = "неизвестный";
+        }
+        return protocol;
+    }
+
+    public static String checkDomain(String url) {
+        //напишите тут ваш код
+        String domain = "";
+        if (url.endsWith("com")) {
+            domain = "com";
+        } else if (url.endsWith("net")) {
+            domain = "net";
+        } else if (url.endsWith("org")) {
+            domain = "org";
+        } else if (url.endsWith("ru")) {
+            domain = "ru";
+        } else {
+            domain = "неизвестный";
+        }
+        return domain;
+    }
+}
