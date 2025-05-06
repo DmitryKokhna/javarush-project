16b27911875a7cd8a98b49953045febb4a4f5ca6
DmitryKokhna <dmitry.kokhna@gmail.com>
2025-05-06 21:41:17 +0300

    06_May_2025 com.javarush.task.pro.task10.task1020

---
+++ src/com/javarush/task/pro/task10/task1020/Computer.java
@@ -0,0 +1,9 @@
+package com.javarush.task.pro.task10.task1020;
+
+public class Computer {
+SystemUnit systemUnit = new SystemUnit();
+Monitor monitor = new Monitor();
+Mouse mouse = new Mouse();
+Keyboard keyboard = new Keyboard();
+
+}
