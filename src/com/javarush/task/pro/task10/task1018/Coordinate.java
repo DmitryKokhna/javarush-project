4b45fa89c1cac4f228609e7f1ad3bca0ef09883b
DmitryKokhna <dmitry.kokhna@gmail.com>
2025-05-10 15:45:10 +0300

    10_May_2025 com.javarush.task.pro.task10.task1018 "Геттер и сеттер в деле"

---
+++ src/com/javarush/task/pro/task10/task1018/Coordinate.java
@@ -0,0 +1,24 @@
+package com.javarush.task.pro.task10.task1018;
+
+public class Coordinate {
+    private int x;
+    private int y;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        //ТУТ изначаьно было x=this.x;
+        this.x= x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        //ТУТ изначаьно было x=this.x;
+        this.y = y;
+    }
+}
