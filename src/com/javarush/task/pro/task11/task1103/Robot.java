9a0509962514847fd472914ff8b55bb781e5a7ae
DmitryKokhna <dmitry.kokhna@gmail.com>
2025-05-18 21:35:54 +0300

    18_May_2025 com.javarush.task.pro.task11.task1103 "Презентация роботов"

--- src/com/javarush/task/pro/task11/task1103/Robot.java
+++ src/com/javarush/task/pro/task11/task1103/Robot.java
@@ -7,12 +7,12 @@ package com.javarush.task.pro.task11.task1103;
 public class Robot {
 
     public static int robotCounter;
-
-    public static int id;
-
-    public static String model;
-
-    public static int productionYear;
+    // ТУТ static убран т.к. ID уникальный для объекта
+    public int id;
+    // ТУТ static убран т.к. MODEL уникальный для объекта
+    public String model;
+    // ТУТ static убран т.к. ГОД уникальный для объекта
+    public int productionYear;
 
     public static String vendor = "Javarush Co.";
 
@@ -24,6 +24,6 @@ public class Robot {
 
     public void displayInformation() {
         System.out.printf("Я %d-й из %d роботов. Модель: %s. Произведен компанией %s в %d году.\n",
-                 id, robotCounter, model, vendor, productionYear);
+                id, robotCounter, model, vendor, productionYear);
     }
 }
