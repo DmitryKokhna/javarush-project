ef32329d8c4774f0530d42a9a6aec5a7df036b39
DmitryKokhna <dmitry.kokhna@gmail.com>
2025-05-23 23:21:29 +0300

    23_May_2025 com.javarush.task.pro.task11.task1117 "Пакет нужен?"

---
+++ src/com/javarush/task/pro/task11/task1117/Solution.java
@@ -0,0 +1,11 @@
+package com.javarush.task.pro.task11.task1117;
+// ТУТ добавили ПАКЕТ, которого не было
+/*
+Пакет нужен?
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("TestPackage");
+    }
+}
