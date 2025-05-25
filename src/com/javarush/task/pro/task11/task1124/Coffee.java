a887eb3cb11bc8da31bd44e2329500e1193c9e37
DmitryKokhna <dmitry.kokhna@gmail.com>
2025-05-25 21:22:42 +0300

    25_May_2025 com.javarush.task.pro.task11.task1124 "MacCoffee 3 в 1"

---
+++ src/com/javarush/task/pro/task11/task1124/Coffee.java
@@ -0,0 +1,14 @@
+package com.javarush.task.pro.task11.task1124;
+
+/* 
+MacCoffee 3 в 1
+*/
+
+public class Coffee {
+    public static class Milk {
+    }
+}
+//ТУТ УБРАЛИ модификатор "public" т.к. только один класс В файле может иметь тип "public"
+// вложенные классы могут
+class Sugar {
+}
