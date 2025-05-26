11abe1e649c2d571759e910bb69479f42365cf13
DmitryKokhna <dmitry.kokhna@gmail.com>
2025-05-26 20:54:06 +0300

    26_May_2025 com.javarush.task.pro.task11.task1126 "Студент"

---
+++ src/com/javarush/task/pro/task11/task1126/Student.java
@@ -0,0 +1,14 @@
+package com.javarush.task.pro.task11.task1126;
+
+/* 
+Студент
+*/
+
+public class Student {
+    //ТУТ изменили тип с pritate НА public
+    public String name;
+// ТУТ убрали VOID и добаввили THIS
+    public Student(String name) {
+        this.name = name;
+    }
+}
