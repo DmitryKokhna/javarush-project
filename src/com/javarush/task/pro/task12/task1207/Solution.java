14a3b5ca511f42cc036b8c5a7ab11b16390329db
DmitryKokhna <dmitry.kokhna@gmail.com>
2025-05-29 23:56:31 +0300

    29_May_2025 com.javarush.task.pro.task12.task1208 "Сравнение символов"

---
+++ src/com/javarush/task/pro/task12/task1207/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.pro.task12.task1207;
+
+/* 
+Какое число меньше
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int first = 2;
+        int second = 1;
+
+        Boolean isLess = isLess(first, second);
+
+        if (isLess) {
+            System.out.println("Первое число меньше второго");
+        } else {
+            System.out.println("Первое число больше или равно второму числу");
+        }
+    }
+
+    public static Boolean isLess(int first, int second) {
+        //ТУТ изчачально было присваиване NULL "first < second ? true :false;"
+        // ИСПРАВЛЕНО на "false"
+        return first < second ? true :false;
+    }
+}
