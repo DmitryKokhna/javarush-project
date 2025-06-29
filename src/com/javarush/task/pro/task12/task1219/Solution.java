bf7f786f1aaca996e7cb3b6939ee1e3ced60e6ba
DmitryKokhna <dmitry.kokhna@gmail.com>
2025-06-29 23:08:20 +0300

    29_June_2025 com.javarush.task.pro.task12.task1219 "Двойные фигурные скобки"

---
+++ src/com/javarush/task/pro/task12/task1219/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.pro.task12.task1219;
+
+import java.util.ArrayList;
+
+/* 
+Двойные фигурные скобки
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        var strings = new ArrayList<String>()
+            // ТУТ добавили двойные {{ }} скобки и точка с запятой переместились в конец объеяления
+        {{
+                add("Так");
+                add("тоже");
+                add("можно");
+                add("делать");
+                add("!");
+            }};
+
+        }
+    }
+
