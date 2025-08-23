5dff4b89aeff21706d164de22e9f3f53ab57a649
DmitryKokhna <dmitry.kokhna@gmail.com>
2025-08-24 00:01:00 +0300

    23_Aug_2025 com.javarush.task.pro.task13.task1305 "Найти и обезвредить" add comments

--- src/com/javarush/task/pro/task13/task1305/Solution.java
+++ src/com/javarush/task/pro/task13/task1305/Solution.java
@@ -39,7 +39,11 @@ public class Solution {
 
     public static void removeBugWithFor(ArrayList<String> list) {
         //напишите тут ваш код
-        String s;
+        //ТУТ объявляем переменную для сравнения
+                String s;
+                //циклом проходим по Списку и переводим в нижний регистр
+        // если элемент найден, то удаяем его
+        // сдвигаем Индекс на минус 1, т.к. список стал меньше
         for (int i = 0; i < list.size(); i++) {
             s = list.get(i).toLowerCase();
             if (s.equals("bug")) {
@@ -52,9 +56,13 @@ public class Solution {
 
     public static void removeBugWithWhile(ArrayList<String> list) {
         //напишите тут ваш код
+        //ТУТ через Итератор перебираем Список
         Iterator<String> it = list.iterator();
-        while (it.hasNext())
-        {
+        // через цикл проверяем есть ли элементы в Списке
+        // ложим элемент в Список
+        // переводми в нижний регистр
+        // сравниваем по значению и удаляем по значению
+        while (it.hasNext()) {
             String str = it.next();
             String strLowerCase = str.toLowerCase();
             if (strLowerCase.equals("bug"))
@@ -64,10 +72,13 @@ public class Solution {
 
     public static void removeBugWithCopy(ArrayList<String> list) {
         //напишите тут ваш код
+        //ТУТ создаем копию списка
         ArrayList<String> copyList = new ArrayList<>(list);
-
+         //объявляем перменную под нижний регистр
         String lowerCase;
-
+          //Перебираем Список
+        //  элемент переводим в нижний регистр
+        // удаляем по значению
         for (String string : copyList) {
             lowerCase = string.toLowerCase();
             if (lowerCase.equals("bug")) {
